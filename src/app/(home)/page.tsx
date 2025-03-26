ca946d6a97b0409441da90cc474cae546814b026
FaRusDev <22978769+FaRusDev@users.noreply.github.com>
2025-03-26 21:39:16 +0700

    build: finishing sidebar and navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image"
-
 export default function Home() {
   return (
     <div>
-      <Image src="logo.svg" alt="tes" width={50} height={50}></Image>
+      <h1>This will load videos</h1>
     </div>
   )
 }
